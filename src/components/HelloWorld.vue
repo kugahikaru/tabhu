dd506230cc49679e49e9096a47e3250f03651f9a
kugahikaru <7318hikky@gmail.com>
2020-03-10 04:20:03 +0900

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,61 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container fluid>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <v-row justify="center">
+          <h1 class="display-2 font-weight-bold mb-3">
+            TABHU
+          </h1>
+        </v-row>
+        <v-row justify="center">
+          <p>
+            major: add 2     ex)CM7 ... cm72<br />
+            aug  : add aug   ex)Caug ... caug<br />
+            minorM7: add mm7 ex)CmM7 ... cmm7<br />
+            flat: add b      ex)Cm7♭5 ... cm7b5<br />
+            add: add add     ex)Cadd9 ... cadd9<br />
+            sharp: add 3     ex)C#7 ...c37<br />
+          </p>
+        </v-row>
+        <v-textarea
+          outlined
+          name="chords"
+          label="Outlined textarea"
+          v-model="chords"
+          @keyup="createTabImg"
+        >
+        </v-textarea>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col v-for="(chord, index) in chordsArray" v-bind:key="index">
+        <v-card flat tile>
+          <v-img
+            v-if="chord"
+            :src="'https://muuu.jp/images/' + chord + '-s.png'"
+            max-width="240"
+            max-height="180"
+          ></v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return{
+        chords: "",
+        chordsArray: []
+      }
+    },
+    methods: {
+      createTabImg: function(){
+        console.log(this.chords);
+        this.chordsArray = this.chords.split(",");
+      }
+    }
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
